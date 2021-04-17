ed4dcfbadfb17c65e0fc6b915369f9ede7c62c9a
Andrew Lambert <andbron@users.noreply.github.com>
2021-04-17 14:37:53 +1000

    Update Install-Applications.ps1

--- Install-Applications.ps1
+++ Install-Applications.ps1
@@ -22,7 +22,7 @@ function Write-Log {
 
 #region Foxit Reader
 try {
-    Start-Process -filepath msiexec.exe -Wait -ErrorAction Stop -ArgumentList '/i', 'c:\temp\FoxitReader101_enu_Setup.msi', '/quiet', 'ADDLOCAL="FX_PDFVIEWER"'
+    Start-Process -filepath msiexec.exe -Wait -ErrorAction Stop -ArgumentList '/i', 'c:\temp\FoxitReader1013_enu_Setup.msi', '/quiet', 'ADDLOCAL="FX_PDFVIEWER"'
     if (Test-Path "C:\Program Files (x86)\Foxit Software\Foxit Reader\FoxitReader.exe") {
         Write-Log "Foxit Reader has been installed"
     }
