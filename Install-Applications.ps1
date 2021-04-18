ce3335f346ced53febd439c7d5200ebc646a76ec
Andrew Lambert <andbron@users.noreply.github.com>
2021-04-18 10:36:50 +1000

    Update Install-Applications.ps1

--- Install-Applications.ps1
+++ Install-Applications.ps1
@@ -20,6 +20,18 @@ function Write-Log {
 }
 #endregion
 
+#region Disable Internet Explorer ESC 
+try {
+     $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+     Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+     Stop-Process -Name Explorer
+}
+catch {
+    $ErrorMessage = $_.Exception.message
+    write-log "Error disabling IE ESC: $ErrorMessage"
+}
+#endregion
+
 #region Foxit Reader
 try {
     Start-Process -filepath msiexec.exe -Wait -ErrorAction Stop -ArgumentList '/i', 'c:\temp\FoxitReader1013_enu_Setup.msi', '/quiet', 'ADDLOCAL="FX_PDFVIEWER"'
